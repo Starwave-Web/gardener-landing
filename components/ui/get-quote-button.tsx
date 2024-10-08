a546d7f1c1e87d96c80f4b3a84af9b555e85dd52
udvardiendre <udvardiendre@gmail.com>
2024-10-08 20:36:53 +0200

    get quote button

--- components/ui/get-quote-button.tsx
+++ components/ui/get-quote-button.tsx
@@ -3,10 +3,11 @@
 import { scrollToSection } from "@/lib/utils";
 import React from "react";
 import { Button, type ButtonProps } from "./button";
+import { SECTIONS } from "@/constants";
 
 const GetQuoteButton = (props: ButtonProps) => {
   return (
-    <Button {...props} onClick={() => scrollToSection("contactUs")}>Ajánlatkérés</Button>
+    <Button {...props} onClick={() => scrollToSection(SECTIONS.CONTACT_US)}>Ajánlatkérés</Button>
   );
 };
 
